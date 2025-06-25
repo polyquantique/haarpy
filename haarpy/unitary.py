a51b2157883c49aa00c599cd16183bfc771e0eba
yaniccd <yanic.cardin.1@gmail.com>
2025-06-25 01:02:14 -0400

    orthogonal files

--- haarpy/unitary.py
+++ haarpy/unitary.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """
-haarpy Python interface
+Unitary group Python interface
 """
 
 from math import factorial, prod
