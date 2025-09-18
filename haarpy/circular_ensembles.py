f7c0d747166bf1af9789063accc4e24fcddc8e72
yaniccd <yanic.cardin.1@gmail.com>
2025-09-18 20:24:41 +0900

    circular ensembles

---
+++ haarpy/circular_ensembles.py
@@ -0,0 +1,55 @@
+# Copyright 2025 Polyquantique
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Circular ensembles Python interface
+"""
+
+from fractions import Fraction
+from functools import lru_cache
+from sympy import Symbol
+from sympy.combinatorics import Permutation
+from haarpy import weingarten_orthogonal, weingarten_symplectic
+
+
+@lru_cache
+def weingarten_circular_orthogonal(
+    permutation: Permutation, coe_dimension: Symbol
+) -> Symbol:
+    """Returns the circular orthogonal ensembles Weingarten functions
+
+    Args:
+        permutation (Permutation): A permutation of the symmetric group S_2k
+        coe_dimension (int): The dimension of the COE
+
+    Returns:
+        Symbol : The Weingarten function
+    """
+    return weingarten_orthogonal(permutation, coe_dimension + 1)
+
+
+@lru_cache
+def weingarten_circular_symplectic(
+    permutation: Permutation, cse_dimension: Symbol
+) -> Symbol:
+    """Returns the circular symplectic ensembles Weingarten functions
+
+    Args:
+        permutation (Permutation): A permutation of the symmetric group S_2k
+        cse_dimension (int): The dimension of the CSE
+
+    Returns:
+        Symbol : The Weingarten function
+    """
+    return weingarten_symplectic(permutation, (2*cse_dimension -1)/2)
+    #return Fraction(2*cse_dimension-1, 2) if isinstance(cse_dimension, Symbol) else 1
---
+++ haarpy/tests/test_circular_ensembles.py
@@ -0,0 +1,81 @@
+# Copyright 2025 Polyquantique
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Circular ensembles tests
+"""
+
+import pytest
+from sympy import Symbol, simplify
+from sympy.combinatorics import SymmetricGroup
+import haarpy as ap
+
+d = Symbol('d')
+
+
+@pytest.mark.parametrize("half_degree", range(1,3))
+def test_weingarten_circular_orthogonal_hyperoctahedral_symbolic(half_degree):
+    """Symbolic validation of COE Weingarten function against results shown in
+    `Matsumoto. Weingarten calculus for matrix ensembles associated with compact symmetric spaces: 
+    <https://arxiv.org/abs/1301.5401>`_
+    """
+    if half_degree == 1:
+        for permutation in SymmetricGroup(2*half_degree).generate():
+            assert ap.weingarten_circular_orthogonal(permutation, d) == 1/(d+1)
+    else:
+        for permutation in SymmetricGroup(2*half_degree).generate():
+            hyperoctahedral = ap.hyperoctahedral(half_degree)
+            coefficient = 1/(d*(d+1)*(d+3))
+            assert ap.weingarten_circular_orthogonal(permutation, d) == (
+                simplify((d+2)*coefficient) if permutation in hyperoctahedral
+                else -coefficient
+            )
+
+
+@pytest.mark.parametrize("half_degree", range(1,3))
+def test_weingarten_circular_orthogonal_hyperoctahedral_numeric(half_degree):
+    """Symbolic validation of COE Weingarten function against results shown in
+    `Matsumoto. Weingarten calculus for matrix ensembles associated with compact symmetric spaces: 
+    <https://arxiv.org/abs/1301.5401>`_
+    """
+    assert False
+
+
+@pytest.mark.parametrize("half_degree", range(1,3))
+def test_weingarten_circular_symplectic_hyperoctahedral_symbolic(half_degree):
+    """Symbolic validation of CSE Weingarten function against results shown in
+    `Matsumoto. Weingarten calculus for matrix ensembles associated with compact symmetric spaces: 
+    <https://arxiv.org/abs/1301.5401>`_
+    """
+    if half_degree == 1:
+        for permutation in SymmetricGroup(2*half_degree).generate():
+            assert ap.weingarten_circular_symplectic(permutation, d) == (
+                permutation.signature()/(2*d-1)
+            )
+    else:
+        for permutation in SymmetricGroup(2*half_degree).generate():
+            hyperoctahedral = ap.hyperoctahedral(half_degree)
+            coefficient = permutation.signature()/(d*(2*d-1)*(2*d-3))
+            assert ap.weingarten_circular_symplectic(permutation, d) == (
+                simplify((d-1)*coefficient) if permutation in hyperoctahedral
+                else coefficient/2
+            )
+
+
+@pytest.mark.parametrize("half_degree", range(1,3))
+def test_weingarten_circular_symplectic_hyperoctahedral_numeric(half_degree):
+    """Symbolic validation of CSE Weingarten function against results shown in
+    `Matsumoto. Weingarten calculus for matrix ensembles associated with compact symmetric spaces: 
+    <https://arxiv.org/abs/1301.5401>`_
+    """
+    assert False
