5eb19584425d57b5d6e19578bc5cab1d69ef6787
yaniccd <yanic.cardin.1@gmail.com>
2025-09-19 10:10:38 +0900

    permutation matrices

---
+++ haarpy/permutation.py
@@ -0,0 +1,38 @@
+# Copyright 2025 Polyquantique
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Permutation matrices Python interface
+"""
+
+def set_partition():
+    return
+
+
+def zeta_function():
+    return
+
+
+def mobius_function():
+    """as seen in Collin & Nagatsu "Weingarten calculus for centered
+    random permutation matrices"
+    """
+    return
+
+
+def weingarten_permutation():
+    return
+
+
+def haar_integral_permutation():
+    return
