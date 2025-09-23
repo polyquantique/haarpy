1a846637503f8ad0cb6863d30d1fcd36de2de1c9
yaniccd <yanic.cardin.1@gmail.com>
2025-09-23 19:59:04 +0900

    join operation

--- haarpy/permutation.py
+++ haarpy/permutation.py
@@ -73,25 +73,41 @@ def partial_order(partition_1: tuple[tuple], partition_2: tuple[tuple]) -> bool:
     return True
 
 
-def partition_intersection(
-    partition_1: tuple[tuple], partition_2: tuple[tuple]
-) -> Generator[int, None, None]:
-    """
-    """
-    flatten_partitions = (
-        tuple(i for j in partition for i in j)
-        for partition in (partition_1, partition_2)
-    )
-    if any(len(flatten) != len(set(flatten)) for flatten in flatten_partitions):
-        raise ValueError("The partitions must be composed of unique elements")
-    
-    partition_set_1 = tuple(set(block) for block in partition_1)
-    partition_set_2 = tuple(set(block) for block in partition_2)
+def meet_operation(partition_1: tuple[tuple], partition_2: tuple[tuple]) -> tuple[tuple]:
+    """Meet operation of two partitions
 
-    return tuple(
-        sum(1 for block_1 in partition_set_1 if block_1 & block_2)
-        for block_2 in partition_set_2
-    )
+    Args:
+        partition_1 (tuple(tuple)): A partition of a set
+        partition_2 (tuple(tuple)): A partition of a set
+
+    Return:   
+    """
+    #RAISE VALUE ERROR IF NOT SAME PARTITION K
+    return
+
+
+def join_operation(partition_1: tuple[tuple], partition_2: tuple[tuple]) -> tuple[tuple]:
+    """
+    """
+    partition_1 = tuple(set(part) for part in partition_1)
+    partition_2 = tuple(set(part) for part in partition_2)
+
+    joined_list = []
+    for block_1 in partition_1:
+        intersection = block_1
+        for block_2 in partition_2:
+            if intersection & block_2:
+                intersection.update(block_2)
+            joined = False
+            for index, block in enumerate(joined_list):
+                if intersection & block:
+                    joined_list[index].update(intersection)
+                    joined = True
+                    break
+            if not joined:
+                joined_list.add(intersection)
+
+    return tuple(tuple(block) for block in joined_list)
 
 
 def mobius_function(
@@ -107,13 +123,32 @@ def mobius_function(
     Returns:
         int: The value of the Möbius function
     """
+    flatten_partitions = (
+        tuple(i for j in partition for i in j)
+        for partition in (partition_1, partition_2)
+    )
+    if any(len(flatten) != len(set(flatten)) for flatten in flatten_partitions):
+        raise ValueError("The partitions must be composed of unique elements")
+    
+    partition_set_1 = tuple(set(block) for block in partition_1)
+    partition_set_2 = tuple(set(block) for block in partition_2)
+
+    partition_intersection = tuple(
+        sum(1 for block_1 in partition_set_1 if block_1 & block_2)
+        for block_2 in partition_set_2
+    )
+
     return prod(
         (-1)**(block_count -1) * factorial(block_count - 1)
-        for block_count in partition_intersection(partition_1, partition_2)
+        for block_count in partition_intersection
     )
 
 
-def weingarten_permutation():
+def weingarten_permutation(
+    first_partition: tuple[tuple[int]],
+    second_partition: tuple[tuple[int]],
+    permutation_size: int,
+):
     #1. generate sigma^tau
     #2. Generate set partitions of each idividual block
     #3. Itertool product all block to find all partial order
